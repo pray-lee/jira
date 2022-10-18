effcc0ec3a1a2019fac378e401d417189e1641c0
pray-lee <praycis@gmail.com>
2022-10-18 12:15:52 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 11111
+          Learn React
         </a>
       </header>
     </div>
